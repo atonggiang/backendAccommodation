aaa90b0979d2dc339de68413f2c6176523d0c02e
Tuan Thanh <atonggiang0410@gmail.com>
2020-12-22 23:12:46 +0700

    token

--- backend/app/views/custom_view.py
+++ backend/app/views/custom_view.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 from .. import models
 from .. import serializers
 from .. import constants
@@ -293,8 +294,13 @@ def get_user_bookmarks_action(request, *args, **kwargs):
 def login_authentication(request, *args, **kwargs):
     result = serializers.LoginSerializer(data=request.data)
     result.is_valid(raise_exception=True)
+    
+    user = models.User.objects.get(username=request.data['username'])
+    refresh = RefreshToken.for_user(user)
     response = {
         'username': result.data['username'],
         'role' : result.data['role'],
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
     }
     return Response(response, status=status.HTTP_200_OK)
